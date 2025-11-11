22ac88568fdd906146f2b63ff3be202fdb33c2f6
square <admin@squarecode.de>
2025-11-11 21:09:16 +0100

    Removed Config, Event and Inventory implementation

--- src/main/java/de/feelix/leviathan/event/Listener.java
+++
@@ -1,7 +0,0 @@
-package de.feelix.leviathan.event;
-
-/**
- * Marker interface for listener classes that contain methods annotated with {@link Subscribe}.
- */
-public interface Listener {
-}
--- src/main/java/de/feelix/leviathan/exceptions/InventoryException.java
+++
@@ -1,9 +0,0 @@
-package de.feelix.leviathan.exceptions;
-
-/**
- * Generic runtime exception for the inventory API subsystem.
- */
-public class InventoryException extends RuntimeException {
-    public InventoryException(String message) { super(message); }
-    public InventoryException(String message, Throwable cause) { super(message, cause); }
-}
